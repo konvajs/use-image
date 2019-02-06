0ac4fdea30fb217f5e694f6a635cd95b9478bb7f
Anton Lavrenov <lavrton@gmail.com>
2019-02-06 15:41:29 -0500

    add todo

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ module.exports = function useImage(url) {
     function() {
       if (!url) return;
       const img = document.createElement('img');
+
+      // TODO: remove listeners on unmount
       img.addEventListener('load', function() {
         setState({ image: img, status: 'loaded' });
       });
