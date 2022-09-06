b72f96303a35c26bf131251f4755df63e44f07a9
Khem Sattaur <hello@khems.net>
2022-09-06 11:19:32 +0100

    Add referrerpolicy

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 declare function useImage(
   url: string,
-  crossOrigin?: 'anonymous' | 'use-credentials'
+  crossOrigin?: 'anonymous' | 'use-credentials',
+  referrerpolicy?: 'no-referrer' | 'no-referrer-when-downgrade' | 'origin' | 'origin-when-cross-origin' | 'same-origin' | 'strict-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url'
 ): [undefined | HTMLImageElement, 'loaded' | 'loading' | 'failed'];
 
 export default useImage;
